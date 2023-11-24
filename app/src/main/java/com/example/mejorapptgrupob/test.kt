815c6af313ecc546c664f3e56f2c245e304b980a
Sergio Guijarro <90042979+sergiky@users.noreply.github.com>
2023-11-24 14:01:07 +0100

    Probando

--- app/src/main/java/com/example/mejorapptgrupob/test.kt
+++ app/src/main/java/com/example/mejorapptgrupob/test.kt
@@ -1,2 +1,9 @@
 package com.example.mejorapptgrupob
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+internal fun test(){
+    Text(text ="Prueba")
+}
