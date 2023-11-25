bdb6211a8c8583dcdc47ae88cfac726bc104ab9d
Sergio Guijarro <90042979+sergiky@users.noreply.github.com>
2023-11-25 12:11:38 +0100

    first steps

---
+++ app/src/main/java/com/example/mejorapptgrupob/screens/mainScreen/MainScreen.kt
@@ -0,0 +1,13 @@
+package com.example.mejorapptgrupob.screens.mainScreen
+
+import androidx.compose.runtime.Composable
+
+class MainScreen {
+    companion object{
+
+        @Composable
+        internal fun mainLayout(){
+
+        }
+    }
+}
