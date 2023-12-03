cbd4dc4a35ee5000daa95c993fa2c0388893441f
Sergio Guijarro <90042979+sergiky@users.noreply.github.com>
2023-12-03 14:26:23 +0100

    test

--- app/src/main/java/com/example/mejorapptgrupob/screens/firstScreen/FirstScreen.kt
+++ app/src/main/java/com/example/mejorapptgrupob/screens/firstScreen/FirstScreen.kt
@@ -50,9 +50,11 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.mejorapptgrupob.R
+import com.example.mejorapptgrupob.screens.registerScreen.RegisterLayout
 import kotlin.math.exp
 
 class FirstScreen {
