309f011d998f905bba20b218a1c694b454fbd0dd
Sakura Díaz <carmen.dna@gmail.com>
2023-12-04 10:17:58 +0100

    Correción de fallos de la Base de Datos Interna

--- app/src/main/java/com/example/mejorapptgrupob/internalDB/DBUtilities.kt
+++ app/src/main/java/com/example/mejorapptgrupob/internalDB/DBUtilities.kt
@@ -1,62 +1,67 @@
 package com.example.mejorapptgrupob.internalDB
 
 import android.content.ContentValues
+import android.content.Context
 import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
 import java.io.ByteArrayOutputStream
 import java.io.IOException
 import java.io.InputStream
 
 
-class DBUtilities (private val inputStream: InputStream)  {
 
-    companion object {
-        fun inicializar(inputStream: InputStream) {
-            val texto = leerArchivo(inputStream)
-            val bh = DBHelper(this)
-            val db = bh.writableDatabase
-            db.beginTransaction()
-            for (i in texto.indices) {
-                if (texto[i] !== "") {
-                    val linea = texto[i].split(";".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
-                    val cvalue = ContentValues()
-                    cvalue.put("Id", linea[0])
-                    cvalue.put("Pregunta", linea[1])
-                    cvalue.put("factor", linea[2])
-                    db.insert("Preguntas", null, cvalue)
-                }
-            }
-            db.setTransactionSuccessful()
-            db.endTransaction()
-        }
+class DBUtilities (private val inputStream: InputStream, context: Context)  {
 
-        private fun leerArchivo(myInputStream: InputStream): Array<String> {
-            // Read the contents of the InputStream
-            val byteArrayOutputStream = ByteArrayOutputStream()
-            try {
-                var i = myInputStream.read()
-                while (i != -1) {
-                    byteArrayOutputStream.write(i)
-                    i = myInputStream.read()
-                }
-                myInputStream.close()
-            } catch (io: IOException) {
-                io.printStackTrace()
-            }
-            return byteArrayOutputStream.toString().split("\n".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
-        }
 
-        private fun obtenerPregunta(i: Int) {
-            val bh = DBHelper(this)
-            val dbR: SQLiteDatabase = bh.readableDatabase
-            val c = dbR.rawQuery("SELECT Pregunta,factor FROM Preguntas Where Id = $i", null)
-            if (c.moveToFirst()) {
-                do {
-                    val pregunta = c.getString(0)
-                    val factor = c.getInt(1)
-                } while (c.moveToNext())
+    private val context: Context = context
+
+    fun inicializar(inputStream: InputStream) {
+        val texto = leerArchivo(inputStream)
+        val bh = DBHelper(context)
+        val db = bh.writableDatabase
+        db.beginTransaction()
+        for (i in texto.indices) {
+            if (texto[i] !== "") {
+                val linea = texto[i].split(";".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+                val cvalue = ContentValues()
+                cvalue.put("Id", linea[0])
+                cvalue.put("Pregunta", linea[1])
+                cvalue.put("factor", linea[2])
+                db.insert("Preguntas", null, cvalue)
             }
-            c.close()
-            dbR.close()
         }
+        db.setTransactionSuccessful()
+        db.endTransaction()
     }
+
+    private fun leerArchivo(myInputStream: InputStream): Array<String> {
+        // Read the contents of the InputStream
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        try {
+            var i = myInputStream.read()
+            while (i != -1) {
+                byteArrayOutputStream.write(i)
+                i = myInputStream.read()
+            }
+            myInputStream.close()
+        } catch (io: IOException) {
+            io.printStackTrace()
+        }
+        return byteArrayOutputStream.toString().split("\n".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+    }
+
+    private fun obtenerPregunta(i: Int) {
+        val bh = DBHelper(context)
+        val dbR: SQLiteDatabase = bh.readableDatabase
+        val c = dbR.rawQuery("SELECT Pregunta,factor FROM Preguntas Where Id = $i", null)
+        if (c.moveToFirst()) {
+            do {
+                val pregunta = c.getString(0)
+                val factor = c.getInt(1)
+            } while (c.moveToNext())
+        }
+        c.close()
+        dbR.close()
+    }
+
 }
--- app/src/main/java/com/example/mejorapptgrupob/internalDB/SQLiteOpenHelper.kt
+++ app/src/main/java/com/example/mejorapptgrupob/internalDB/SQLiteOpenHelper.kt
@@ -1,19 +1,25 @@
 package com.example.mejorapptgrupob.internalDB
 
+import android.content.ContentValues
+import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.io.InputStream
 
 private const val SQL_CREATE_ENTRIES1 = "CREATE TABLE IF NOT EXISTS preguntas" +
         "(Id integer PRIMARY KEY, Pregunta text, factor integer)"
 private const val SQL_CREATE_ENTRIES2 = "CREATE TABLE IF NOT EXISTS " +
         "respuestas(intento integer PRIMARY KEY AUTOINCREMENT,nick text, " +
-        "factor1 integer, factor2 integer,factor3 integer)"
+        "factor1 integer, factor2 integer,factor3 integer,factor4 integer)"
 private const val SQL_DELETE_ENTRIES1 = "DROP TABLE IF EXISTS preguntas"
 private const val SQL_DELETE_ENTRIES2 = "DROP TABLE IF EXISTS respuestas"
 
 
-    class DBHelper(context: DBUtilities.Companion) :
-    SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+    class DBHelper(context: Context) :
+        SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
         override fun onCreate(db: SQLiteDatabase) {
             db.execSQL(SQL_CREATE_ENTRIES1)
             db.execSQL(SQL_CREATE_ENTRIES2)
@@ -30,4 +36,5 @@ private const val SQL_DELETE_ENTRIES2 = "DROP TABLE IF EXISTS respuestas"
             const val DATABASE_VERSION = 1
             const val DATABASE_NAME = "p1_preguntas.db"
         }
+
 }
