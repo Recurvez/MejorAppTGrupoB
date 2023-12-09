e486020efb030ffe46034eb455fd1ac1c0cf8722
Recurvez <whoisandrew21@gmail.com>
2023-12-09 12:06:02 +0100

    user menu done

--- app/src/main/java/com/example/mejorapptgrupob/screens/firstScreen/FirstActivity.kt
+++ app/src/main/java/com/example/mejorapptgrupob/screens/firstScreen/FirstActivity.kt
@@ -4,18 +4,22 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -23,6 +27,12 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExitToApp
+import androidx.compose.material.icons.outlined.Assessment
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.ModeNight
+import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
@@ -32,6 +42,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -46,6 +57,7 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -53,11 +65,18 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.mejorapptgrupob.MainActivity
 import com.example.mejorapptgrupob.MainLayout
 import com.example.mejorapptgrupob.R
 import com.example.mejorapptgrupob.screens.firstScreen.ui.theme.MejorAppTGrupoBTheme
+import com.example.mejorapptgrupob.screens.firstScreen.ui.theme.Purple40
 import com.example.mejorapptgrupob.screens.infoScreen.InfoActivity
 import com.example.mejorapptgrupob.screens.registerScreen.RegisterActivity
+import com.example.mejorapptgrupob.screens.testScreen.TestActivity
+import com.example.mejorapptgrupob.screens.testScreen.ui.theme.Pink40
+import com.example.mejorapptgrupob.screens.testScreen.ui.theme.Pink80
+import com.example.mejorapptgrupob.screens.testScreen.ui.theme.Purple80
+import com.example.mejorapptgrupob.screens.testScreen.ui.theme.PurpleGrey80
 import com.example.mejorapptgrupob.screens.userGuideScreen.UserGuideActivity
 
 class FirstActivity : ComponentActivity() {
@@ -99,50 +118,157 @@ internal fun FirstLayout(){
         // Icono de arriba a la derecha
         var isMenuExpanded by remember { mutableStateOf(false) }
 
+        // Menú de usuario
+        Box(
+            modifier = Modifier,
+            contentAlignment = Alignment.TopEnd
+        ){
+            DropdownMenu(
+                expanded = isMenuExpanded,
+                onDismissRequest = { isMenuExpanded = false },
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .background(
+                        brush = Brush.linearGradient(
+                            colors = listOf(
+                                Color(0xFFB3CCEC), // Azul claro
+                                Color(0xFF8AA1D0), // Azul medio
+                                Color(0xFF5E75B4), // Azul oscuro
+                            ),
+                        )
+                    )
+                ) {
+                Text(
+                    text = "Ajustes de Usuario",
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .padding(8.dp)
+                )
+
+                /*
+                DropdownMenuItem(
+                    modifier = Modifier
+                        .padding(horizontal = 8.dp, vertical = 4.dp)
+                        .background(color= Purple80, shape = RoundedCornerShape(20.dp)),
+                    text = {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.spacedBy(8.dp)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Outlined.Person,
+                                contentDescription = null,
+                                modifier = Modifier.size(24.dp)
+                            )
+                            Text(
+                                text = "Cambiar de Usuario",
+                            )
+                        }
+                    },
+                    onClick = { /* TO-DO */ }
+                )
+                */
+                DropdownMenuItem(
+                    modifier = Modifier
+                        .padding(horizontal = 8.dp, vertical = 4.dp)
+                        .background(color = Purple80, shape = RoundedCornerShape(20.dp)),
+                    text = {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.spacedBy(8.dp)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Outlined.Assessment,
+                                contentDescription = null,
+                                modifier = Modifier.size(24.dp)
+                            )
+                            Text(
+                                text = "Resultados anteriores",
+                            )
+                        }
+                    },
+                    onClick = { /* TO-DO */ }
+                )
+
+                DropdownMenuItem(
+                    modifier = Modifier
+                        .padding(horizontal = 8.dp, vertical = 4.dp)
+                        .background(color = Purple80, shape = RoundedCornerShape(20.dp)),
+                    text = {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.spacedBy(8.dp)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Outlined.ModeNight,
+                                contentDescription = null,
+                                modifier = Modifier.size(24.dp)
+                            )
+                            Text(
+                                text = "Cambiar tema",
+                            )
+                        }
+                    },
+                    onClick = { /* TO-DO */ }
+                )
+
+                DropdownMenuItem(
+                    modifier = Modifier
+                        .padding(horizontal = 8.dp, vertical = 4.dp)
+                        .background(color = Purple80, shape = RoundedCornerShape(20.dp)),
+                    text = {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.spacedBy(8.dp)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Outlined.Info,
+                                contentDescription = null,
+                                modifier = Modifier.size(24.dp)
+                            )
+                            Text(
+                                text = "Información de la app",
+                            )
+                        }
+                    },
+                    onClick = { /* TO-DO */ }
+                )
+
+                DropdownMenuItem(
+                    modifier = Modifier
+                        .padding(horizontal = 8.dp, vertical = 4.dp)
+                        .background(color = PurpleGrey80, shape = RoundedCornerShape(20.dp)),
+                    text = {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.spacedBy(8.dp)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.ExitToApp,
+                                contentDescription = null,
+                                modifier = Modifier.size(24.dp)
+                            )
+                            Text(
+                                text = "Cerrar sesión",
+                            )
+                            }
+                           },
+                    onClick = { mContext.startActivity(Intent(mContext, MainActivity::class.java)) }
+                )
+            }
+        }
+
         CircularIconButton(
             modifier = Modifier
-                .align(Alignment.End)
+                .align(Alignment.Start)
                 .padding(8.dp),
-            icon = painterResource(id = R.drawable.ic_launcher_foreground),
             onClick = {
                 isMenuExpanded = !isMenuExpanded
             }
         )
 
-        // Menú de usuario
-        DropdownMenu(
-            expanded = isMenuExpanded,
-            onDismissRequest = { isMenuExpanded = false },
-            modifier = Modifier
-                .fillMaxHeight()
-                .border(1.dp, MaterialTheme.colorScheme.primary)
-        ) {
-            Text(
-                text = "Menú de Usuario",
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier
-                    .align(Alignment.CenterHorizontally)
-                    .fillMaxSize()
-                    .background(Color(0xFF9370DB))
-            )
-            Divider(color = Color.Black, thickness = 1.dp)
-            DropdownMenuItem(
-                text = { Text("Cambiar de Usuario") },
-                onClick = { /*TODO*/ }
-            )
-            DropdownMenuItem(
-                text = { Text("Ver resultados anteriores") },
-                onClick = { /*TODO*/ }
-            )
-            DropdownMenuItem(
-                text = { Text("Cambiar Tema") },
-                onClick = { /*TODO*/ }
-            )
-            DropdownMenuItem(
-                text = { Text("Información de la app") },
-                onClick = { /*TODO*/ }
-            )
-        }
+
 
         // Espacio entre el icono y el título
         Spacer(modifier = Modifier.height(40.dp))
@@ -192,7 +318,7 @@ internal fun FirstLayout(){
                     start = Offset(0f, 0f),
                     end = Offset.Infinite
                 ),
-                onClick = {/*Por hacer*/}
+                onClick = {mContext.startActivity(Intent(mContext, TestActivity::class.java))}
             )
 
             Spacer(modifier = Modifier.height(30.dp))
@@ -219,23 +345,20 @@ internal fun FirstLayout(){
 @Composable
 fun CircularIconButton(
     modifier: Modifier = Modifier,
-    icon: Painter,
     onClick: () -> Unit
-){
-
+) {
     IconButton(
         onClick = onClick,
         modifier = modifier
-            .size(48.dp)
+            .size(50.dp)
             .clip(CircleShape)
-            .background(color = Color.Black)
-
-    ){
+            .background(color = Purple40)
+    ) {
         Icon(
-            painter = icon,
+            imageVector = Icons.Outlined.Person,
             contentDescription = null,
             tint = Color.White,
-            modifier = Modifier.size(24.dp)
+            modifier = Modifier.size(30.dp)
         )
     }
 }
